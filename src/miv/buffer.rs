49a140a7582388a05b050a88b9d1b708186c9e5e
Xavier Perez <duosrx@gmail.com>
2016-09-26 15:08:06 -0700

    Add some documentation for Buffer

--- src/miv/buffer.rs
+++ src/miv/buffer.rs
@@ -4,14 +4,20 @@ use std::cmp;
 
 use point::Point;
 
-/// # BUFFER STUFF
-/// Heya
+/// Holds the text data and the filepath, if any.
+/// This buffer has no knowledge of the editor.
 pub struct Buffer {
+    /// Holds the actual data.
+    /// This is a *very* inefficient data structure and will eventually be replace by a GapBuffer or Rope.
+    /// This will also end up private so don't really too much on it.
     pub data: Vec<Vec<char>>,
+
+    /// The file path associated with that buffer.
     pub filepath: Option<String>,
 }
 
 impl Buffer {
+    /// Make a new empty `Buffer`.
     pub fn new() -> Buffer {
         Buffer {
             data: vec!(Vec::new()),
@@ -19,15 +25,18 @@ impl Buffer {
         }
     }
 
-    // Number of line in the buffer (minimum 1)
+    /// Number of line in the buffer. Returns 1 if the buffer is empty.
     pub fn line_len(&self) -> usize {
         cmp::max(1, self.data.len())
     }
 
+    /// Find the line at the given row.
     pub fn line_at(&mut self, y: usize) -> Option<&Vec<char>> {
         self.data.get(y)
     }
 
+    /// Find the last column that is non empty.
+    /// Returns 0 if the whole line is empty.
     pub fn last_non_empty_col(&mut self, location: Point) -> usize {
         let x = self.line_at(location.y)
             .and_then(|line| match line.get(location.x) {
@@ -38,18 +47,22 @@ impl Buffer {
         cmp::max(0, x) as usize
     }
 
+    /// Find a char at a specific column/row.
     pub fn char_at(&mut self, location: Point) -> Option<&char> {
         self.line_at(location.y).and_then(|line| line.get(location.x))
     }
 
+    /// Insert a row at the specified point, pushing the other characters to the right.
     pub fn insert(&mut self, location: Point, c: char) {
         self.data[location.y].insert(location.x, c);
     }
 
+    /// Replace a specific character.
     pub fn replace(&mut self, location: Point, c: char) {
         self.data[location.y][location.x] = c;
     }
 
+    /// Replace a character at the given location or insert it.
     pub fn upsert(&mut self, location: Point, c: char) {
         let existing = self.data[location.y][location.x];
 
@@ -60,29 +73,37 @@ impl Buffer {
         }
     }
 
+    /// Delete a character at the given location, shifting the rest of the line to the left.
     pub fn delete(&mut self, location: Point) -> char {
         self.data[location.y].remove(location.x)
     }
 
+    /// Insert several characters at the given location. See `insert`.
     pub fn insert_text(&mut self, location: Point, chars: Vec<char>) {
         for (x, &c) in chars.iter().enumerate() {
             self.data[location.y].insert(x + location.x, c);
         }
     }
 
+    /// Insert an empty line at the given location, shifting the subsequent lines down if any.
     pub fn new_line(&mut self, location: Point) {
         self.data.insert(location.y + 1, vec!('\n'));
     }
 
+    /// Delete a line at the given location, shifting the subsequent lines up if any.
     pub fn delete_line(&mut self, location: Point) -> Vec<char> {
         self.data.remove(location.y)
     }
 
+    /// Split a line in half, inserting the second half as a new line below the first half.
     pub fn split_line(&mut self, location: Point) {
         let newline = self.data[location.y].split_off(location.x);
         self.data.insert(location.y + 1, newline);
     }
 
+    /// Load a file from a path and populate the internal data buffer.
+    /// # Panics
+    /// When the file can't be loaded, e.g. if it doesn't exist.
     pub fn load_file(&mut self, path: String) {
         let mut file = OpenOptions::new().read(true).write(true).create(true).open(path).unwrap();
         let mut s = String::new();
@@ -105,6 +126,13 @@ impl Buffer {
         self.data = buf;
     }
 
+    /// Save the buffer at the internal filepath, returning the number of bytes written.
+    ///
+    /// Changes the line ending to CR, disregarding what they were originally.
+    ///
+    /// Probably doesn't handle UTF-8 very well du to the `char` to `u8` conversion happening.
+    /// # Panics
+    /// When no filepath was given when creating the `Buffer`.
     pub fn save_file(&mut self) -> u64 {
         if self.filepath.is_none() { return 0 } // TODO: choose filepath
 
--- src/miv/view.rs
+++ src/miv/view.rs
@@ -17,43 +17,59 @@ const BAR_HEIGHT: usize = 2;
 
 pub struct View<'a> {
     rustbox: &'a RustBox,
-
-    top_line: usize, // highest visible buffer line
-
-    width: usize,  // entire width (including bottom bar)
-    height: usize, // entire height (including bottom bar)
-
-    window_height: usize, // buffer window height
-    window_width: usize,  // buffer window width
+    /// Highest visible buffer line
+    topline: usize,
+    /// Entire width (including bottom bar)
+    width: usize,
+    /// Entire height (including bottom bar)
+    height: usize,
+    /// Buffer window height
+    window_height: usize,
+    /// Buffer window width
+    window_width: usize,
+    /// Index of the last row before the bottom bar
+    last_row: usize,
+    /// The absolute cursor position (as oppose to the cursor position inside the buffer)
+    cursor: Point,
 }
 
+/// The editor frontend.
+///
+/// Holds a reference to a `RustBox` instance created by the main app.
 impl<'a> View<'a> {
     pub fn new(rustbox: &'a RustBox) -> View {
         View {
             rustbox: rustbox,
-            top_line: 0,
+            topline: 0,
             width: 0,
             height: 0,
             window_height: 0,
             window_width: 0,
+            last_row: 0,
+            cursor: Point::new(0, 0),
         }
     }
 
+    /// Renders the whole editor into the termbox: text, bottom bar, cursor... etc
     pub fn render(&mut self, state: &State) {
-        self.height = state.height - BAR_HEIGHT - 1; // Room for the status line
-        self.width = state.width - 1;
+        self.height = state.height;
+        self.width = state.width;
+        self.window_height = state.height - BAR_HEIGHT;
+        self.window_width = state.width - 1;
+        self.last_row  = self.window_height - 1;
 
-        // Adjust `top_line` for scrolling vertically.
-        if state.cursor.y < self.top_line {
-            self.top_line = state.cursor.y;
-        } else if state.cursor.y > self.height + self.top_line {
-            self.top_line = state.cursor.y - self.height;
+        if state.cursor.y < self.topline {
+            self.topline = state.cursor.y;
+        } else if state.cursor.y > self.last_row + self.topline {
+            self.topline = state.cursor.y - self.last_row;
         }
+
+        self.cursor = self.adjusted_cursor(state.cursor);
 
         self.rustbox.clear();
         self.fill_background(state);
 
-        for (y, line) in state.buffer.data.iter().skip(self.top_line).take(self.height + 1).enumerate() {
+        for (y, line) in state.buffer.data.iter().skip(self.topline).take(self.window_height).enumerate() {
             for (x, &character) in line.iter().enumerate() {
                 if character == '\n' { continue };
                 self.rustbox.print_char(x, y, rustbox::RB_NORMAL, FG_COLOR, BG_COLOR, character);
@@ -67,10 +83,10 @@ impl<'a> View<'a> {
         self.rustbox.present();
     }
 
-    // The cursor passed by State is the absolute position in the text buffer.
-    // This adjusts the position by `top_line`. Used for vertical scrolling.
+    /// The cursor passed by State is the absolute position in the text buffer.
+    /// This adjusts the position by `top_line`. Used for vertical scrolling.
     fn adjusted_cursor(&self, cursor: Point) -> Point {
-        Point { x: cursor.x, y: cursor.y - self.top_line }
+        Point { x: cursor.x, y: cursor.y - self.topline }
     }
 
     fn print_bar(&self, state: &State) {
@@ -78,7 +94,7 @@ impl<'a> View<'a> {
             let keys: String = state.keystrokes.iter()
                 .filter_map(|&k| key_to_string(k).or(None))
                 .collect();
-            self.rustbox.print(18, self.rustbox.height() - 2, rustbox::RB_BOLD, Color::White, BAR_BG_COLOR, keys.as_ref());
+            self.rustbox.print(18, self.window_height, rustbox::RB_BOLD, Color::White, BAR_BG_COLOR, keys.as_ref());
         }
 
         self.print_coords(state);
@@ -88,18 +104,18 @@ impl<'a> View<'a> {
 
     fn print_mode(&self, state: &State) {
         let mode = format!(" {}  ", state.mode().display);
-        self.rustbox.print(0, self.rustbox.height() - 2, rustbox::RB_BOLD, BAR_FG_COLOR, Color::Byte(state.mode().color), mode.as_ref());
+        self.rustbox.print(0, self.window_height, rustbox::RB_BOLD, BAR_FG_COLOR, Color::Byte(state.mode().color), mode.as_ref());
     }
 
     fn print_coords(&self, state: &State) {
         let coords = format!("  {}:{}  ", state.cursor.y + 1, state.cursor.x);
         let color = Color::Byte(state.mode().color);
-        self.rustbox.print(self.rustbox.width() - 1 - coords.len(), self.rustbox.height() - 2, rustbox::RB_BOLD, BAR_FG_COLOR, color, coords.as_ref());
+        self.rustbox.print(self.window_width - coords.len(), self.window_height, rustbox::RB_BOLD, BAR_FG_COLOR, color, coords.as_ref());
     }
 
     fn print_status(&self, state: &State) {
         if let Some(status) = state.status.clone() {
-            self.rustbox.print(0, self.rustbox.height() - 1, rustbox::RB_BOLD, Color::White, BG_COLOR, status.as_ref());
+            self.rustbox.print(0, self.window_height, rustbox::RB_BOLD, Color::White, BG_COLOR, status.as_ref());
         }
     }
 
@@ -112,7 +128,7 @@ impl<'a> View<'a> {
         }
 
         // Info bar
-        let y = self.rustbox.height() - 2;
+        let y = self.window_height;
         let bg_color = self.bar_bg_color(state);
         for x in 0..self.rustbox.width() {
             self.rustbox.print(x, y, rustbox::RB_NORMAL, Color::White, bg_color, " ");
